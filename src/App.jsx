07bccce869e9a802ed2d080e1d85d644e09481f3
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-08-04 18:00:17 -0400

    fetched players successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+
+  const apiUrl =
+    "https://fsa-puppy-bowl.herokuapp.com/api/2306-fsa-et-web-ft-sf/players";
+
+  useEffect(() => {
+    const fetchPuppies = async () => {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      
+      setPlayers(data.data.players);
+    }
+
+    fetchPuppies()
+  }, []);
+
+  console.log(players)
 
   return (
     <>
       <h1>Puppy Bowl</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
