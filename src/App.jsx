ea20ee50bbfef11e348b1b8ff5ccbcaadaf7e43b
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2023-08-06 23:09:25 -0400

    form component shows up yay

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Router from "./components/Router.jsx";
 import "./App.css";
+import Form from "./components/Form.jsx";
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Puppy Bowl</h1>
+      <Form />
       <Router />      
     </>
   );
